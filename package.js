0a168e67bc6d22cb6601efe9694e2c34ce71a798
Ryan Glover <me@ryanglover.net>
2013-05-10 10:09:36 -0500

    Initial commit. Porting glDatePicker to a Meteorite package.

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "Meteor implementation of the glDatePicker plugin for jQuery."
+});
+
+Package.on_use(function(api){
+
+  api.add_files([
+    'lib/glDatePicker/styles/glDatePicker.darkneon.css',
+    'lib/glDatePicker/styles/glDatePicker.default.css',
+    'lib/glDatePicker/styles/glDatePicker.flatwhite.css',
+    'lib/glDatePicker/glDatePicker.min.js'
+  ], 'client');
+  
+});
